8f44e767b161c7c098bf0d5b41cfbb74e6403153
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-03-17 19:18:00 +0200

    feat: trying db reconfig

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: true,
+      ssl: env("DATABASE_SSL", false),
     },
   },
 });
