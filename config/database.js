d267d30b484835444155ee236d7774c78b3c186f
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-03-21 09:46:22 +0200

    feat: test

--- config/database.js
+++ config/database.js
@@ -9,7 +9,9 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
