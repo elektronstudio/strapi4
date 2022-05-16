c496bbc381dd99e2525474c06ca56ca0acf5e5cd
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-16 16:00:33 +0300

    cleanup

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,25 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", env("DO_SPACE_CDN")],
+          "media-src": ["'self'", "data:", "blob:", env("DO_SPACE_CDN")],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
