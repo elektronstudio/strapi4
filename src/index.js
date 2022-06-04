2f33b9cb9f814e819127ddd6c08ba5a5b28275c8
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-05 00:35:37 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -7,25 +7,38 @@ module.exports = {
 
   bootstrap({ strapi }) {
     const wss = new WebSocket.Server({ server: strapi.server.httpServer });
+    strapi.sendMessage = (message) =>
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(message));
+        }
+      });
+    strapi.storeMessage = async (message) => {
+      await strapi.entityService.create("api::message.message", {
+        data: {
+          channel: message.channel,
+          type: message.type,
+          user_id: message.userid, // userid
+          user_name: message.username, // username
+          value: message.value,
+        },
+      });
+    };
+    strapi.wss = wss;
+
     wss.on("connection", (ws) => {
-      ws.on("message", (message) => {
-        let parsedMessage = null;
+      ws.on("message", (rawMessage) => {
+        let message = null;
         try {
-          parsedMessage = JSON.parse(message.toString());
+          message = JSON.parse(rawMessage.toString());
         } catch {}
-        if (parsedMessage) {
-          wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify(parsedMessage));
-            }
-          });
+        if (message) {
+          strapi.sendMessage(message);
+          if (message.store) {
+            strapi.storeMessage(message);
+          }
         }
       });
     });
-    strapi.wss = wss;
-
-    // strapi.entityService
-    //   .findMany("api::project.project")
-    //   .then((res) => console.log(res));
   },
 };
