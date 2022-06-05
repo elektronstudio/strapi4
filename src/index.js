26be74b3a274df59b5ab871047d47f8bd53d55b2
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-05 23:10:48 +0300

    feat: get stats

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const WebSocket = require("ws");
+const { $fetch } = require("ohmyfetch");
+// const { $fetch } = ohmyfetch.default;
 
 module.exports = {
   register() {},
@@ -41,5 +43,24 @@ module.exports = {
         }
       });
     });
+
+    const url =
+      "https://vds27aojo9.execute-api.eu-north-1.amazonaws.com/default/GetElektronStats";
+    const xApiKey = "ftLPhkyMwY8ilqqrmzd9n9HyeYsgXKq7aROIucY9";
+
+    const getStats = async () => {
+      const stats = await $fetch(url, { headers: { "x-api-key": xApiKey } });
+      if (stats.length) {
+        const message = {
+          type: "STATS",
+          value: stats,
+          channel: "elektron",
+        };
+        strapi.sendMessage(message);
+        strapi.storeMessage(message);
+      }
+    };
+    getStats();
+    setInterval(getStats, 1000 * 60);
   },
 };
