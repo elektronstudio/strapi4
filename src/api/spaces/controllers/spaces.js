2132dbdabc8967b568603e4bc1d0594e750bcb44
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-07-01 13:29:29 +0300

    cleanup

--- src/api/spaces/controllers/spaces.js
+++ src/api/spaces/controllers/spaces.js
@@ -4,12 +4,11 @@ const { writeFile } = require("fs/promises");
 module.exports = {
   async index(ctx, _next) {
     ctx.request.accepts("application/json");
-    ctx.response.send({ status: "OK" });
+    ctx.response.send(strapi.config.get("plugin.upload.providerOptions"));
   },
   async upload(ctx, _next) {
     ctx.request.accepts("application/json");
     const buffer = imageDataURI.decode(ctx.request.body.src).dataBuffer;
-    //const file = await imageDataURI.outputFile(ctx.request.body.src, filePath);
     await writeFile("test2.jpg", buffer);
     ctx.response.send(ctx.request.body);
   },
