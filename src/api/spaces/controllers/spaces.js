7ec3cde8310f1ccf8e1e6525ac91a6000c2a335a
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-07-01 13:11:44 +0300

    fixes

--- src/api/spaces/controllers/spaces.js
+++ src/api/spaces/controllers/spaces.js
@@ -1,13 +1,16 @@
-const unparsed = require("koa-body/unparsed.js");
-const body = require("koa-body");
+const imageDataURI = require("image-data-uri");
+const { writeFile } = require("fs/promises");
 
 module.exports = {
   async index(ctx, _next) {
-    //ctx.request.accepts("application/json");
-    ctx.response.send("OK");
+    ctx.request.accepts("application/json");
+    ctx.response.send({ status: "OK" });
   },
   async upload(ctx, _next) {
-    console.log(ctx.request.body);
+    ctx.request.accepts("application/json");
+    const buffer = imageDataURI.decode(ctx.request.body.src).dataBuffer;
+    //const file = await imageDataURI.outputFile(ctx.request.body.src, filePath);
+    await writeFile("test2.jpg", buffer);
     ctx.response.send(ctx.request.body);
   },
 };
--- src/api/spaces/routes/spaces.js
+++ src/api/spaces/routes/spaces.js
@@ -8,25 +8,25 @@ module.exports = {
       handler: "spaces.index",
     },
     {
-      method: "POST",
+      method: "PUT",
       path: "/spaces/upload",
       handler: "spaces.upload",
-      config: {
-        middlewares: [
-          {
-            name: "strapi::body",
-            config: {
-              formLimit: "256mb",
-              jsonLimit: "256mb",
-              textLimit: "256mb",
-              formLimit: "256mb",
-              formidable: {
-                maxFileSize: 256 * 1024 * 1024,
-              },
-            },
-          },
-        ],
-      },
+      // config: {
+      //   middlewares: [
+      //     {
+      //       name: "strapi::body",
+      //       config: {
+      //         formLimit: "256mb",
+      //         jsonLimit: "256mb",
+      //         textLimit: "256mb",
+      //         formLimit: "256mb",
+      //         formidable: {
+      //           maxFileSize: 256 * 1024 * 1024,
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
     },
   ],
 };
