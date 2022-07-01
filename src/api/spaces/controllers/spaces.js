a778a4a7634a6d5bc85617078dc5b820fbdcc025
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-07-01 14:03:40 +0300

    yea

--- src/api/spaces/controllers/spaces.js
+++ src/api/spaces/controllers/spaces.js
@@ -1,15 +1,39 @@
 const imageDataURI = require("image-data-uri");
+const Spaces = require("do-spaces");
 const { writeFile } = require("fs/promises");
+
+const options = strapi.config.get("plugin.upload.providerOptions");
+const spaces = new Spaces.Spaces({
+  endpoint: options.endpoint,
+  accessKey: options.key,
+  secret: options.secret,
+  bucket: options.space,
+});
+
+// https://elektron.fra1.digitaloceanspaces.com/captures/hello.jpg
 
 module.exports = {
   async index(ctx, _next) {
     ctx.request.accepts("application/json");
-    ctx.response.send(strapi.config.get("plugin.upload.providerOptions"));
+    const files = await spaces.listFiles({
+      path: `captures/`,
+    });
+    const processFile = (file) => {
+      return `https://${options.cdn}/${file.Key}`;
+    };
+    ctx.response.send(files.Contents.map(processFile));
   },
   async upload(ctx, _next) {
     ctx.request.accepts("application/json");
-    const buffer = imageDataURI.decode(ctx.request.body.src).dataBuffer;
-    await writeFile("test2.jpg", buffer);
+    const file = imageDataURI.decode(ctx.request.body.src).dataBuffer;
+    //await writeFile("test2.jpg", buffer);
+    const s = await spaces.uploadFile({
+      pathname: "captures/hello2.jpg",
+      privacy: "public-read",
+      file,
+    });
+    console.log(s);
+
     ctx.response.send(ctx.request.body);
   },
 };
--- src/api/spaces/routes/spaces.js
+++ src/api/spaces/routes/spaces.js
@@ -1,5 +1,3 @@
-const body = require("koa-body");
-
 module.exports = {
   routes: [
     {
@@ -11,22 +9,6 @@ module.exports = {
       method: "PUT",
       path: "/spaces/upload",
       handler: "spaces.upload",
-      // config: {
-      //   middlewares: [
-      //     {
-      //       name: "strapi::body",
-      //       config: {
-      //         formLimit: "256mb",
-      //         jsonLimit: "256mb",
-      //         textLimit: "256mb",
-      //         formLimit: "256mb",
-      //         formidable: {
-      //           maxFileSize: 256 * 1024 * 1024,
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
     },
   ],
 };
