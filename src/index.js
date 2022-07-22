ff48cd6ac93e536a80cd34d119670c82dbc517b9
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-07-22 12:24:55 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,50 @@
-'use strict';
+"use strict";
+
+const WebSocket = require("ws");
 
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+  register() {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    const wss = new WebSocket.Server({ server: strapi.server.httpServer });
+
+    strapi.sendMessage = (message) =>
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(message));
+        }
+      });
+
+    strapi.storeMessage = async (message) => {
+      await strapi.entityService.create("api::message.message", {
+        data: {
+          channel: message.channel,
+          type: message.type,
+          userid: message.userid,
+          username: message.username,
+          value: message.value,
+          datetime: message.datetime || new Date(),
+          created_by_id: 1,
+          updated_by_id: 1,
+        },
+      });
+    };
+
+    strapi.wss = wss;
+
+    wss.on("connection", (ws) => {
+      ws.on("message", (rawMessage) => {
+        let message = null;
+        try {
+          message = JSON.parse(rawMessage.toString());
+        } catch {}
+        if (message) {
+          strapi.sendMessage(message);
+          if (message.store) {
+            strapi.storeMessage(message);
+          }
+        }
+      });
+    });
+  },
 };
