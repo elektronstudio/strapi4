c78bea2133c8c63bdcf87dc51007c8eb4dcd680e
sven-ra <mail@svenraju.net>
2022-12-15 16:44:24 +0200

    feat: anthropologies-of-space collections

---
+++ src/api/anthropologies-of-space/controllers/anthropologies-of-space.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  anthropologies-of-space controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::anthropologies-of-space.anthropologies-of-space');
---
+++ src/api/anthropologies-of-space/services/anthropologies-of-space.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anthropologies-of-space service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::anthropologies-of-space.anthropologies-of-space');
